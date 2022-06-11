42a53f8aa88ffd80a1720c9c9522a7ef75d99f81
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-11 18:25:51 +0200

    dodano wstepna zawartosc i wymiary ContentPage

--- showcase-music-star/src/containers/ContactPage.jsx
+++ showcase-music-star/src/containers/ContactPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+import "./ContactPage.css";
 
 const ContactPage = () => {
+    const navigate = useNavigate();
     return (
         <div className="ContactPage">
+            <p className="close" onClick={() => navigate(-1)}>&#10006;</p>
             <p>Contact Page</p>
         </div>
     );
