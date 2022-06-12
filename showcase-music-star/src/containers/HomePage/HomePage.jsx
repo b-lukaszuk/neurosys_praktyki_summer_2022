bc6b8d31cf256e4c13a909266d0030c6465f3834
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-12 19:28:14 +0200

    kosmetyka

--- showcase-music-star/src/containers/HomePage/HomePage.jsx
+++ showcase-music-star/src/containers/HomePage/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     return (
         <div className="HomePage">
             <h1>Strona główna</h1>
-            <p>Cześć, tu Ed Sheeran</p>
+            <p>Cześć, tu Ed Sheeran.</p>
             <p>Zapraszam do obejrzenia klipów moich ulubionych piosenek. Warto :)</p>
             <VideoCarousel />
         </div>
